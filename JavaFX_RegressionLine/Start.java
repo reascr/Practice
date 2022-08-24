6820c119cda5ee834c7f564364fd33a55e6abdf1
reascr <94176469+reascr@users.noreply.github.com>
2022-08-24 14:57:54 +0200

    Update Start.java

--- JavaFX_RegressionLine/Start.java
+++ JavaFX_RegressionLine/Start.java
@@ -14,19 +14,3 @@ public class Start
 		Application.launch(Screen.class); 
 	}
 }
-
-
-// points:
-//
-// create point:				1.5/1.5
-// create multiple points:		1/1.0
-// calculate slope:				1.5/1.5
-// calculate intersection:		1/1.0
-// draw regression line:		2/2.0
-// update regression line:		1/1.0
-// 
-// style:						2/2.0
-//
-// total:						10/10.0
-
-// well done
